9a4898e286e00d83506dbfbbbea3ad20d61b9498
DESKTOP-UVQUJPQ\mulle <mortenlarsen16@gmail.com>
2017-12-07 14:42:18 +0100

    Tilføjelse af kode

--- Mypage.php
+++ Mypage.php
@@ -1,9 +1,20 @@
 <!DOCTYPE html>
 <html>
+
 <head>
+
+<link rel="stylesheet" type="text/css" href="Mypage.css">
+
 	<title>Mypage</title>
+
 </head>
+
 <body>
-<h1>Hvem er jeg??</h1>
+
+<h1>Pokémon</h1>
+
+<center><iframe width="560" height="315" src="https://www.youtube.com/embed/O70v5OuqP2I?autoplay=1" frameborder="0" allowfullscreen></iframe></center>
+
 </body>
+
 </html>
