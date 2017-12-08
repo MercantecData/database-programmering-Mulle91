4b4663f78c28dde74acea12e2afce6796504f350
DESKTOP-UVQUJPQ\mulle <mortenlarsen16@gmail.com>
2017-12-08 11:03:05 +0100

    Rettelser

---
+++ login.php
@@ -0,0 +1,28 @@
+<?php
+    include "db.php";
+    $userN = $_POST["username"];
+    $pass = $_POST["password"];
+
+    $sql = "SELECT * FROM logininfo WHERE `username`= '$userN'";
+
+    $result = mysqli_query($conn, $sql);
+
+    if (mysqli_num_rows($result) > 0) {
+        // output data of each row
+        while($row = mysqli_fetch_assoc($result)) {
+            if($row["username"] == $userN && $row["password"] == $pass) 
+            {
+                echo "You are currect logged in " . $row["username"];
+                header ("location: Mypage.php");
+            } else {
+                echo "It's not the correct password or username";
+
+            }
+            //echo "id: " . $row["id"]. " - Name: " . $row["userName"]. " " . $row["password"]. "<br>";
+        }
+    } else {
+        echo "It's not the correct password or username";
+    }
+    
+    mysqli_close($conn);
+?>
